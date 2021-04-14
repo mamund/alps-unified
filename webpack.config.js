fe5a6fe962acc3f2a48986911041cccd7c84ff44
Filip <filip26@gmail.com>
2021-04-14 13:32:15 +0200

    prepare for publishing

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'alps-unified.js',
+    filename: 'unified.js',
   },
+ node: {
+  fs: 'empty',
+}
 };
