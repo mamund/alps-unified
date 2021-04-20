972b929b4840db4aeba110ab852114306db92c42
Filip <filip26@gmail.com>
2021-04-20 15:39:43 +0200

    add tests 2-6, fix open-api.js

--- src/translator.js
+++ src/translator.js
@@ -1,9 +1,13 @@
 
+const canonicalize = require('./canonicalizer');
+
+const toOAS = require('./translator/open-api');
+
 module.exports = {
   toJSON: require('./translator/alps-json'), 
   toSDL: require('./translator/sdl'),
   toAsync: require('./translator/async-api'),
-  toOAS: require('./translator/open-api'),
+  toOAS: (doc, options) => toOAS(canonicalize(doc), options),
   toProto: require('./translator/proto'),
   toWSDL: require('./translator/wsdl'),  
 }
